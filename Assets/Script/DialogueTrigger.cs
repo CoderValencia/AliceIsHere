bc3c5eaed57c4c4f4281fbee2118e7ab337f5e8f
CoderValencia <valencia.coleman625@gmail.com>
2023-07-18 16:55:17 -0400

    Dialogue System Set up

---
+++ Assets/Script/DialogueTrigger.cs
@@ -0,0 +1,118 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Ink.Runtime;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Rendering;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public bool playerInSpace;
+    public TextAsset inkJson;
+    private Story currentStory;
+    public bool dialogueIsPlaying;
+    public TextMeshProUGUI dialogueText;
+    public TextMeshProUGUI dialoguePrompt;
+    public GameObject dialogueBox;
+
+    const string SPEAKER_TAG = "speaker";
+    public TextMeshProUGUI speakerName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueIsPlaying = false;
+        playerInSpace = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerInSpace == true)
+        {
+            EnterDialogueMode(inkJson);
+        }
+        if (Input.GetMouseButtonDown(0) && playerInSpace == true)
+        {
+            ContinueStory();
+            Debug.Log("Continue Story");
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Player is in talking space");
+            playerInSpace = true;
+        }
+    }
+   
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Player is in talking space");
+            playerInSpace = false;
+        }
+    }
+
+    public void EnterDialogueMode(TextAsset inkJson)
+    {
+        currentStory = new Story(inkJson.text);
+        dialogueBox.SetActive(true);
+        dialogueIsPlaying = true;
+
+
+        if (currentStory.canContinue)
+        {
+            dialogueText.text = currentStory.Continue();
+            HandleTags(currentStory.currentTags);
+        }
+        else
+        {
+            ExitDialogue();
+        }
+    }
+
+    void ExitDialogue()
+    {
+        dialogueIsPlaying = false;
+        dialogueBox.SetActive(false);
+        dialogueText.text = "";
+    }
+
+    void ContinueStory()
+    {
+        Debug.Log(currentStory.currentText);
+        if (currentStory.canContinue)
+        {
+            dialogueText.text = currentStory.Continue();
+            HandleTags(currentStory.currentTags);
+        }
+        else
+        {
+            ExitDialogue();
+
+        }
+    }
+
+    void HandleTags(List<string> currentTags)
+    {
+        foreach (string tag in currentTags)
+        {
+            string[] splitTag = tag.Split(":");
+            if (splitTag.Length != 2)
+            {
+                Debug.Log(splitTag.Length);
+                Debug.LogError("Tag could not be parsed:" + tag);
+            }
+            string tagKey = splitTag[0];
+            string tagValue = splitTag[1];
+            speakerName.text = tagValue;
+            Debug.Log(tagValue);
+          
+
+        }
+
+    }
+}
