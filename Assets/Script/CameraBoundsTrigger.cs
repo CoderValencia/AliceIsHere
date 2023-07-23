4ff24594533559ee9bcb3cc5bd3a8560ee2a5b52
rtnman <114710680+rtnman@users.noreply.github.com>
2023-07-23 12:55:35 -0400

    Set up room 1

--- Assets/Script/CameraBoundsTrigger.cs
+++ Assets/Script/CameraBoundsTrigger.cs
@@ -6,12 +6,17 @@ public class CameraBoundsTrigger : MonoBehaviour
 {
     public Vector2 topRightCorner;
     public Vector2 bottomLeftCorner;
+    public float CameraSize;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Camera.main.gameObject.GetComponent<CameraMove>().topRightCorner = topRightCorner;
             Camera.main.gameObject.GetComponent<CameraMove>().bottomLeftCorner = bottomLeftCorner;
+            if(CameraSize > 0)
+            {
+                Camera.main.orthographicSize = CameraSize;
+            }
         }
     }
 }
---
+++ Assets/Script/LoadingZone.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingZone : MonoBehaviour
+{
+    int counter = 0;
+    public string sceneName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            counter = 1;
+            Camera.main.GetComponent<CameraFade>().StartFade();
+            collision.gameObject.GetComponent<PlayerMovement>().enabled = false;
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, collision.gameObject.GetComponent<Rigidbody2D>().velocity.y);
+            collision.gameObject.transform.position = new Vector3(collision.gameObject.transform.position.x, collision.gameObject.transform.position.y, 0);
+        }
+    }
+    void FixedUpdate()
+    {
+        if(counter > 0)
+        {
+            counter++;
+        }
+        if(counter == 50)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
